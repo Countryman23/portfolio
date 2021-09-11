78ee2b46274e39248b891ac76802ea2b994abd77
Rustam <avaraa@mail.ru>
2021-09-11 23:05:27 +0300

    Updates

---
+++ precache-manifest.a127422b712a42142baca8021e4ca383.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6269b80840b878a765a5e99abcace3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "78c61d2c2399c9ee2d9e",
+    "url": "/portfolio/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "3eb2094964b03012af3e",
+    "url": "/portfolio/static/css/main.de55a77f.chunk.css"
+  },
+  {
+    "revision": "78c61d2c2399c9ee2d9e",
+    "url": "/portfolio/static/js/2.078d3356.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.078d3356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb2094964b03012af3e",
+    "url": "/portfolio/static/js/main.1b13eef2.chunk.js"
+  },
+  {
+    "revision": "c442b425a9888c7655db",
+    "url": "/portfolio/static/js/runtime-main.06b13e98.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/portfolio/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/portfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "544a18fd2795bbe9e49c5986c78a53b6",
+    "url": "/portfolio/static/media/avatar.544a18fd.png"
+  },
+  {
+    "revision": "7fb5c5057c5acf8cc7dd50ec2275202f",
+    "url": "/portfolio/static/media/bg.7fb5c505.jpg"
+  },
+  {
+    "revision": "ef2d360f163af3ac74af6c737f38edf6",
+    "url": "/portfolio/static/media/loading.ef2d360f.svg"
+  },
+  {
+    "revision": "2e426d09898ba9e3fc0887da1bd0bc24",
+    "url": "/portfolio/static/media/qr.2e426d09.png"
+  }
+]);
